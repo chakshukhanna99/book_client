10ecad3e4b186417994cca81ccb1f7d93cff9070
Chakshu Khanna <chakshukhanna2110@gmail.com>
2024-04-19 17:35:27 +0530

    fetch data get book list and create books card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Banner from "@/app/(Home)/components/Banner";
+import BookList from "./(Home)/components/BookList";
 
 export default function Home() {
   return (
    <>
    <Banner/>
+   <BookList/>
    <h1>Welcome To books client app</h1>
    </>
   );
