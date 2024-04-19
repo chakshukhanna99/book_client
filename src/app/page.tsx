d557680055ea987eb1486b0f2955230e121a91dd
Chakshu Khanna <chakshukhanna2110@gmail.com>
2024-04-19 14:46:29 +0530

    banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
    <>
+   <Banner/>
    <h1>Welcome To books client app</h1>
    </>
   );
